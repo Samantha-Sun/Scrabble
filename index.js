714a8baad5272a43f72242a94bc213b6b8b23c7e
Samantha-Sun <56792344+Samantha-Sun@users.noreply.github.com>
2019-10-20 15:14:53 +0200

    Add files via upload
    First version of project

---
+++ index.js
@@ -0,0 +1,37 @@
+//decalring values
+const lib = {
+  e:1, a:1, i:1, o:1, n:1, r:1, t:1, l:1, s:1, u:1,
+  d:2, g:2,
+  b:3, c:3, m:3, p:3,
+  f:4, h:4, v:4, w:4, y:4,
+  k:5,
+  j:8, x:8,
+  q:10, z:10
+}
+
+const calcScore = (text) => {
+  let score = 0;
+  text = text.toLowerCase();
+  for (let ch of text)
+  score += lib[ch];
+  return score;
+}
+
+const score = (word) => {
+  let tscore = 0;
+  tscore = calcScore(word);
+  return tscore;
+}
+
+window.onload = function(){
+  let input = document.getElementById("input");
+  let submit = document.getElementById("test");
+  let output = document.getElementById("output");
+  submit.onclick = function() {
+    if (input.value != "") {
+      output.innerHTML = "Scrabble Score: " + score(input.value)
+      } else {
+          output.innerHTML = "Please enter a word"
+      }
+  }    
+}
